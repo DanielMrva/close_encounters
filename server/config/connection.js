2be1b2b1cae69883f76d9486757ceefeb4baf063
Daniel Mrva <mrva.paints@gmail.com>
2022-06-01 18:58:55 -0600

    forgot one of my own todos

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 //TODO: edit NEEDNAME to our db name
 mongoose.connect(
-    process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/NEEDNAME', {
+    process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/close_encounters_DB', {
         newUrlParser: true,
         useUnifiedTopology: true,
     }
