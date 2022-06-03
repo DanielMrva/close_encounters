f174fa2701b6f4f1f8f2538bc0bd2819dec349c2
Daniel Mrva <mrva.paints@gmail.com>
2022-06-02 21:42:27 -0600

    had to do a file reorganization with the create react app

--- client/close-encounters/src/components/basic-map.js
+++ client/close-encounters/src/components/basic-map.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import { MapContainer, TileLayer, useMap } from 'react-leaflet';
+import { MapContainer, TileLayer} from 'react-leaflet';
 import tileLayer from '../utils/tileLayer';
 
 const MapWrapper = () => {
     return (
-        <>
-            <MapContainer center={[50.5, 30.5]} zoom={13}>
+        <MapContainer center={[50.5, 30.5]} zoom={13} scrollWheelZoom={true}>
                 <TileLayer {...tileLayer}/>
-            </MapContainer>
-        </>
+        </MapContainer>
+        
     )
 };
 
--- client/close-encounters/src/index.js
+++ client/close-encounters/src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
+import MapWrapper from './components/basic-map';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <MapWrapper className = "mapHeight"/>
   </React.StrictMode>
 );
 
