ac47c59d4dc810647266c460861ae2d5096c7b68
Daniel Mrva <mrva.paints@gmail.com>
2022-06-04 09:38:10 -0600

    minor map modifications for closer-to-center map marker rendering.

--- client/close-encounters/src/pages/basic-map.js
+++ client/close-encounters/src/pages/basic-map.js
@@ -9,29 +9,29 @@ import tileLayer from '../utils/tileLayer';
 
 const mockData = [
     {
-        lat: 41.7190,
-        lng: -100.1010,
+        lat: 40.7190,
+        lng: -103.1010,
         type: "UFO",
         title: "Cigar UFO Encounter",
         date: "10-21-2020"
     },
     {
-        lat: 38.0078,
-        lng: -110.4587,
+        lat: 39.0078,
+        lng: -102.4587,
         type: "PN",
         title: "Vision of my great grandfather",
         date: "6-19-1999"
     },
     {
         lat: 40.2213,
-        lng: -109.5732,
+        lng: -105.5732,
         type: "CZ",
         title: "Unicorn Sighting",
         date: "12-24-2012"
     },
     {
-        lat: 42.5109,
-        lng: -102.1570,
+        lat: 40.5109,
+        lng: -104.1570,
         type: "UFO",
         title: "Silver Disk UFO signting",
         date: "7-4-1958"
