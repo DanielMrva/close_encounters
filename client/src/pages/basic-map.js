593e3fb88edb92d8ef0521521dfe019b95911c69
Amber Zimmerman <ajz1221@gmail.com>
2022-06-05 19:21:46 -0600

    adding in folders with files for js and cs pages, updated mongo connection lines, updated file path causing server error

--- client/src/pages/basic-map.js
+++ client/src/pages/basic-map.js
@@ -1,19 +1,19 @@
-
-import React from 'react';
-import { MapContainer, TileLayer } from 'react-leaflet';
-import tileLayer from '../../utils/tileLayer';
-
-
+import React from "react";
+import { MapContainer, TileLayer } from "react-leaflet";
+import tileLayer from "../utils/tileLayer";
 
 const MapWrapper = () => {
-    const mapPositions = [39.7392, -104.9903];
+  const mapPositions = [39.7392, -104.9903];
 
-    return (
-        <MapContainer className="map" center={[mapPositions[0], mapPositions[1]]} zoom={10}>
-            <TileLayer {...tileLayer}/>
-        </MapContainer>
-        
-    )
+  return (
+    <MapContainer
+      className="map"
+      center={[mapPositions[0], mapPositions[1]]}
+      zoom={10}
+    >
+      <TileLayer {...tileLayer} />
+    </MapContainer>
+  );
 };
 
 export default MapWrapper;
