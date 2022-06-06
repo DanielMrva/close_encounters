c701352f61c1f00d3cece4a1470494cc56811629
Daniel Mrva <mrva.paints@gmail.com>
2022-06-06 00:55:15 -0600

    now have a function that gets our map bounds (and other info, I got it from a stack overflow sollution, will need to tailor to our needs

--- client/src/pages/basic-map.js
+++ client/src/pages/basic-map.js
@@ -1,18 +1,11 @@
-
 import React from 'react';
 import {useEffect, useState } from 'react';
-import { MapContainer, Rectangle, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, Rectangle, TileLayer, Marker, Popup, useMapEvents } from 'react-leaflet';
 import L from 'leaflet';
 import tileLayer from '../utils/tileLayer';
 // import { UFO, CZ, PN, DF } from '../mapassets';
 
 const mapPositions = [39.7392, -104.9903];
-
-// const mapBounds = MapContainer.getBounds();
-
-// console.log(mapBounds);
-
-
 
 const mockData = [
     {
@@ -52,131 +45,94 @@ const mockData = [
     },
 ];
 
-
-function getRandomColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16)}`.toString();
+function makeACall(bounds, zoom, zoomThreshold = 8) {
+    console.log(`current map zoom is ${zoom}`)
+    if(zoom > zoomThreshold) {
+        console.log(`make a call to the server with the bounds`, bounds)
+    }
 }
 
-const SetRentacle = ({ bounds }) => {
-        return bounds.map((bound, index) => (
-        <Rectangle 
-            key={index}
-            bounds={bound}
-            color={getRandomColor()}
-            weight={10}
-            fillOpacity={0.1} />
-    ));
-}
-
-function contentText(getBounds) {
-const { _northEast, _southWest } = getBounds;
-return `SouthWest: ${_southWest}, NorthEast: ${_northEast}`;
-}
-
-const Location = ({ map }) => {
-    const [bounds, setBounds] =
-    useState([])
-
-    useEffect(() => {
-        if (!map) return;
-
-        const info = L.DomUtil.create('div', 'legend');
-
-        const position = L.Control.extend({
-            options: {
-                position: 'bottomleft'
-            },
-
-            onAdd: function () {
-                info.innerHTML = contentText(map.getBounds());
-                return info;
-            }
-        })
-
-        map.addControl(new position());
-
-        map.on('moveend zoomend', () => {
-            const bounds = map.getBounds();
-            info.textContent = 
-            contentText(bounds);
-            setBounds(b => [...b, bounds])
-        });
-    }, [map])
-
-    return bounds?.length <= 0 ? <SetRentacle bounds={bounds} />
-    : null;
+const MapEvents = () => {
+    const map = useMapEvents({
+        moveend: () => makeACall(map.getBounds(), map.getZoom()),
+        zoomend: () => makeACall(map.getBounds(), map.getZoom())
+    });
+    return null;
 }
 
 //begins our markers from mockData
-// const markerIcon = (type) => {
+const markerIcon = (type) => {
 
-//     // let myIconURL = "";
-//     let myIconColor = "";
+    // let myIconURL = "";
+    let myIconColor = "";
 
-//     switch (type) {
-//         case "UFO":
-//             // myIconURL = '../mapassets/ufo-svgrepo-com.svg'
-//             // myIconURL = UFO;
-//             myIconColor = "#03fcec";
-//             break;
-//         case "CZ":
-//             // myIconURL = '../mapassets/unicorn-svgrepo-com.svg'
-//             // myIconURL = CZ;
-//             myIconColor = "#e77ef2";
-//             break;
-//         case "PN":
-//             // myIconURL = '../mapassets/ghost-svgrepo-com.svg'
-//             // myIconURL = PN;
-//             myIconColor = "#55edb5"
-//             break;
-//         default: 
-//             // myIconURL = 
-//             // myIconURL = DF;
-//             myIconColor = "#000000";
-//     }
+    switch (type) {
+        case "UFO":
+            // myIconURL = '../mapassets/ufo-svgrepo-com.svg'
+            // myIconURL = UFO;
+            myIconColor = "#03fcec";
+            break;
+        case "CZ":
+            // myIconURL = '../mapassets/unicorn-svgrepo-com.svg'
+            // myIconURL = CZ;
+            myIconColor = "#e77ef2";
+            break;
+        case "PN":
+            // myIconURL = '../mapassets/ghost-svgrepo-com.svg'
+            // myIconURL = PN;
+            myIconColor = "#55edb5"
+            break;
+        default: 
+            // myIconURL = 
+            // myIconURL = DF;
+            myIconColor = "#000000";
+    }
 
-//     const svgTemplate = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" class="marker">
-//     <path fill-opacity=".25" d="M16 32s1.427-9.585 3.761-12.025c4.595-4.805 8.685-.99 8.685-.99s4.044 3.964-.526 8.743C25.514 30.245 16 32 16 32z"/>
-//     <path fill="${myIconColor}" stroke="#fff" d="M15.938 32S6 17.938 6 11.938C6 .125 15.938 0 15.938 0S26 .125 26 11.875C26 18.062 15.938 32 15.938 32zM16 6a4 4 0 100 8 4 4 0 000-8z"/>
-//     </svg>`
+    const svgTemplate = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32" class="marker">
+    <path fill-opacity=".25" d="M16 32s1.427-9.585 3.761-12.025c4.595-4.805 8.685-.99 8.685-.99s4.044 3.964-.526 8.743C25.514 30.245 16 32 16 32z"/>
+    <path fill="${myIconColor}" stroke="#fff" d="M15.938 32S6 17.938 6 11.938C6 .125 15.938 0 15.938 0S26 .125 26 11.875C26 18.062 15.938 32 15.938 32zM16 6a4 4 0 100 8 4 4 0 000-8z"/>
+    </svg>`
 
-//     return new L.DivIcon({
-//         className: "test",
-//         html: svgTemplate,
-//         iconSize: [40, 40],
-//         iconAnchor: [20, 20],       
-//         popupAnchor: [-5, -50],
-//     });
-// }
+    return new L.DivIcon({
+        className: "test",
+        html: svgTemplate,
+        iconSize: [40, 40],
+        iconAnchor: [20, 20],       
+        popupAnchor: [-5, -50],
+    });
+}
 
-// const MapMarkers = ({ data }) => {
-//     return data.map((item, index) => (
-//         <Marker 
-//         key={index}
-//         icon={markerIcon(item.type)}
-//         position={{ lat: item.lat, lng: item.lng }}
-//         >
-//           <Popup>{item.type}</Popup>  
-//         </Marker>
-//     ))
-// }
+const MapMarkers = ({ data }) => {
+    return data.map((item, index) => (
+        <Marker 
+        key={index}
+        icon={markerIcon(item.type)}
+        position={{ lat: item.lat, lng: item.lng }}
+        >
+          <Popup>{item.type}</Popup>  
+        </Marker>
+    ))
+}
 
 const MapWrapper = () => {
     const [map, setMap] = useState(null);
 
     return (
     <MapContainer 
-        className="map" 
+        className='map'
         whenCreated={setMap} 
         center={mapPositions} 
         zoom={10}>
+            <MapEvents/>
             <TileLayer {...tileLayer}/>
 
-            {/* <MapMarkers data={mockData} /> */}
+
+            <MapMarkers data={mockData} />
 
             {/* <Legend map={map}/> */}
+            {/* <Location map={map} /> */}
 
-            <Location map={map} />
+
 
         </MapContainer>
         
@@ -270,3 +226,60 @@ export default MapWrapper;
 //     return bounds?.length <= 0 ? <SetRentacle bounds={bounds} />
 //     : null;
 // }
+
+
+
+// function getRandomColor() {
+//     return `#${Math.floor(Math.random() * 16777215).toString(16)}`.toString();
+//     }
+    
+//     const SetRentacle = ({ bounds }) => {
+//     return bounds.map((bound, index) => (
+//         <Rectangle
+//         key={index}
+//         bounds={bound}
+//         color={getRandomColor()}
+//         weight={10}
+//         fillOpacity={0.1} />
+//     ));
+//     }
+    
+//     function contentText(getBounds) {
+//     const { _northEast, _southWest } = getBounds;
+//     return `SouthWest: ${_southWest}, NorthEast: ${_northEast}`;
+//     }
+    
+//     const Location = ({ map }) => {
+//     const [bounds, setBounds] = useState([])
+    
+//     useEffect(() => {
+//         if (!map) return;
+    
+//         const info = L.DomUtil.create('div', 'legend');
+    
+//         const position = L.Control.extend({
+//         options: {
+//             position: 'bottomleft'
+//         },
+    
+//         onAdd: function () {
+//             info.innerHTML = contentText(map.getBounds());
+//             return info;
+//         }
+//         })
+    
+//         map.addControl(new position());
+    
+//         map.on('moveend zoomend', () => {
+//         const bounds = map.getBounds();
+//         info.textContent = contentText(bounds);
+//         setBounds(b => [...b, bounds])
+//         });
+    
+//     }, [map])
+    
+//     return bounds?.length <= 0
+//         ? <SetRentacle bounds={bounds} />
+//         : null;
+//     }
+    
