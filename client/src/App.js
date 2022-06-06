09c13f4d463203e9d7e74607d66ce36c3357df28
Amber Zimmerman <ajz1221@gmail.com>
2022-06-05 22:34:45 -0600

    homepage css and content mostly complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-import MapWrapper from './pages/basic-map';
-import InputPage from './components/Inputs/InputPage'
+// import MapWrapper from "./pages/basic-map";
+// import InputPage from "./components/Inputs/InputPage";
+import Home from "./pages/Home/Home";
+import Topnav from "./components/Topnav/Topnav";
 
 function App() {
   return (
     <div className="App">
-
+      <Topnav />
+      <Home />
       {/* <MapWrapper /> */}
-      <InputPage />
+      {/* <InputPage /> */}
     </div>
   );
 }
