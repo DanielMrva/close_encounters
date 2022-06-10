e80d4ffbda8a89be760557503dea63fb219d113b
Lindsey Choi <lindsey.e.choi@gmail.com>
2022-06-10 14:32:07 -0600

    working on the react router. working, still needs the sign up route and the map route goes to the map page doesn't show the map

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,83 @@
+import "./App.css";
+import React from 'react';
+import { Link } from "react-router-dom";
+import AppContainer from "./AppContainer";
+// import Test from "./pages/Test";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import ReactDOM from "react-dom/client";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import Home from "./pages/Home/Home";
+
+
+const client = new ApolloClient({
+  uri: "/graphql",
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <div className="App">
+        <Home />
+      </div>
+    </ApolloProvider>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // import "./App.css";
 
 // import AppContainer from "./AppContainer";
-// // import Test from "./pages/Test";
-// import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+// import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from "@apollo/client";
+// import { setContext } from '@apollo/client/link/context';
+
+// // Construct our main GraphQL API endpoint
+// const httpLink = createHttpLink({
+//   uri: '/graphql',
+// });
+
+// // Construct request middleware that will attach the JWT token to every request as an `authorization` header
+// const authLink = setContext((_, { headers }) => {
+//   // get the authentication token from local storage if it exists
+//   const token = localStorage.getItem('id_token');
+//   // return the headers to the context so httpLink can read them
+//   return {
+//     headers: {
+//       ...headers,
+//       authorization: token ? `Bearer ${token}` : '',
+//     },
+//   };
+// });
 
 // const client = new ApolloClient({
-//   uri: "/graphql",
+//   // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
+//   link: authLink.concat(httpLink),
 //   cache: new InMemoryCache(),
 // });
 
@@ -21,48 +93,3 @@
 // }
 
 // export default App;
-
-
-import "./App.css";
-
-import AppContainer from "./AppContainer";
-
-import { ApolloClient, ApolloProvider, InMemoryCache, createHttpLink } from "@apollo/client";
-import { setContext } from '@apollo/client/link/context';
-
-// Construct our main GraphQL API endpoint
-const httpLink = createHttpLink({
-  uri: '/graphql',
-});
-
-// Construct request middleware that will attach the JWT token to every request as an `authorization` header
-const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('id_token');
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      authorization: token ? `Bearer ${token}` : '',
-    },
-  };
-});
-
-const client = new ApolloClient({
-  // Set up our client to execute the `authLink` middleware prior to making the request to our GraphQL API
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
-
-function App() {
-  return (
-    <ApolloProvider client={client}>
-      <div className="App">
-        {/* <Test /> */}
-        <AppContainer />
-      </div>
-    </ApolloProvider>
-  );
-}
-
-export default App;
--- client/src/components/Topnav/Topnav.js
+++ client/src/components/Topnav/Topnav.js
@@ -1,50 +1,35 @@
 import "./Topnav.css";
 import Auth from '../../utils/auth';
+import { Link } from "react-router-dom";
 
-export default function Topnav({ currentPage, handlePageChange }) {
+export default function Topnav() {
   const logout = (event) => {
     event.preventDefault();
     Auth.logout();
   };
   return (
     <div className="topnav-flex-container">
-      <a
-        href="#Home"
-        onClick={() => handlePageChange("Home")}
-        className={currentPage === "Home" ? "close active" : "close"}
-      >
+
+      <Link to ="/">
         close encounters
-      </a>
-      <a
-        href="#Encountercard"
-        onClick={() => handlePageChange("Encountercard")}
-        className={currentPage === "Encountercard" ? "close active" : "close"}
-      >
+      </Link>
+
+      <a>
         encountercardtest
       </a>
+
       <div className="icon-container">
-        <a
-          href="#MapWrapper"
-          onClick={() => handlePageChange("MapWrapper")}
-          className={
-            currentPage === "MapWrapper" ? "nav-link active" : "nav-link"
-          }
-        >
+
+        <Link to="/mapwrapper">
           <i className="fa-solid fa-earth-americas fa-2xl"></i>
-        </a>
-        <a
-          href="#User"
-          onClick={() => handlePageChange("User")}
-          className={currentPage === "User" ? "nav-link active" : "nav-link"}
-        >
+        </Link>
+
+        <Link to="/user">
           <i className="fa-solid fa-person-falling fa-2xl"></i>
-        </a>
-{/* this is where the logout icon button goes */}
-        <a
-          href="#User"
-          onClick={() => handlePageChange("User")}
-          className={currentPage === "User" ? "nav-link active" : "nav-link"}
-        >
+        </Link>
+        {/* this is where the logout icon button goes */}
+
+        <a>
           <i className="fa-solid fa-person-falling fa-2xl"></i>
         </a>
 
