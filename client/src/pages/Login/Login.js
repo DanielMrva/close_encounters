2822588d6f17a044c4818f23da59b1e9b2303863
Lindsey Choi <lindsey.e.choi@gmail.com>
2022-06-11 12:43:51 -0600

    merging

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -4,7 +4,7 @@ import { useMutation } from '@apollo/client';
 import { LOGIN_USER } from '../../utils/mutations';
 import Auth from '../../utils/auth';
 import { Link } from "react-router-dom";
-
+// merging
 export default function Login() { 
   const [formState, setFormState] = useState({ email: '', password: '' });
   const [login, { error }] = useMutation(LOGIN_USER);
