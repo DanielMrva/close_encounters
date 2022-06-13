93638bd220c4616ce6ca6f76f80b861e576f126e
Daniel Mrva <mrva.paints@gmail.com>
2022-06-12 23:52:42 -0600

    get out of my face zoom controlls

--- server/server.js
+++ server/server.js
@@ -39,5 +39,10 @@ const startApolloServer = async(typeDefs, resolvers) => {
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
-
+app.get('/*', (req, res) => {
+    let url = path.join(__dirname, '../client/build', 'index.html');
+    if (!url.startsWith('/app/')) // we're on local windows
+      url = url.substring(1);
+    res.sendFile(url);
+  });
 startApolloServer(typeDefs, resolvers);
