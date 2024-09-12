60dc7df50ba97fecec355716a70bc3185e941878
Daniel Mrva <mrva.paints@gmail.com>
2024-09-11 22:03:35 -0600

    fix with index.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import App from "./App";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "leaflet/dist/leaflet.css";
 import "./index.css";
 
